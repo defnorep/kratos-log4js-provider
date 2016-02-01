9055566a8e53b1246e6d27051591728dfe344cd3
Matthew Evans <mcaevans@gmail.com>
2016-02-01 07:25:22 -0700

    Added a default configuration
    Very simple, just console.
    
    Also removed an extra semicolon just for Killswitch.

--- src/provider.js
+++ src/provider.js
@@ -1,13 +1,19 @@
 import { Provider } from 'kratos'
 import log4js from 'log4js'
 
+const defaultConfig = {
+  appenders: [
+    { type: 'console' }
+  ]
+}
+
 export default class Log4JsProvider extends Provider {
   register () {
-    const config = this.config('logs')
+    const config = this.config('logs', defaultConfig)
     log4js.configure(config)
     this.bind('log', () => {
       const logger = log4js.getLogger()
       return logger
-    });
+    })
   }
 }
